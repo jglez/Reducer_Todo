96ad6f75bb954e17facf88128d9ad035e2cf673a
jglez <jglez.cs@gmail.com>
2021-01-19 20:41:37 -0600

    Created todos data, render via .map(), strike through when clicked

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import todos from './data/todos.js'
+
+const complete = (evt) => {
+  evt.target.classList.add('completed')
+  
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Todo</h1>
+
+        {todos.map(todo => {
+          return <div className='tasks' onClick={complete} key={todo.id}>{todo.task}</div>
+        })}
+
+
       </header>
     </div>
   );
---
+++ src/data/todos.js
@@ -0,0 +1,24 @@
+const todos = [
+  {
+    id: 1,
+    task: 'Walk my dog',
+    completed: false
+  },
+  {
+    id: 2,
+    task: 'Wash the dishes',
+    completed: false
+  },
+  {
+    id: 3,
+    task: 'Take out the trash',
+    completed: false
+  },
+  {
+    id: 4,
+    task: 'Clean my room',
+    completed: false
+  }
+]
+
+export default todos
